cc47a868d889a2ba3eb83a645cb67bb517fccb42
Jesse Almanrode <jesse@almanrode.com>
2017-01-14 18:04:55 -0700

    Adding beginnings of setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding=utf-8
+""" Setup script for genders-python module
+"""
+
+from setuptools import setup
+
+setup(name='genders',
+      version='',
+      description='Python module for working with libgenders',
+      author='wcooly',
+      author_email='',
+      url='https://github.com/wcooley/genders-python',
+      py_modules=['genders'],
+      license='',
+      install_requires=[''],
+      platforms=['Linux'],
+      classifiers=['Programming Language :: Python',
+                  'Intended Audience :: Developers',
+                  'Programming Language :: Python',
+                  'Programming Language :: Python :: 2',
+                  'Programming Language :: Python :: 2.7',
+                  'Topic :: Software Development :: Libraries',
+                  'Topic :: Software Development :: Libraries :: Python Modules',
+                  ],
+    )
